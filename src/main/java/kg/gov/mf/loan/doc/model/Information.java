1a3fdfba483ee9e6d54f6eb5aa1c61c1d514bcf8
nurlix <regalix@gmail.com>
2018-01-15 18:24:56 +0600

    1515

--- src/main/java/kg/gov/mf/loan/doc/model/Information.java
+++
@@ -1,9 +0,0 @@
-package kg.gov.mf.loan.doc.model;
-
-import javax.persistence.Entity;
-import javax.persistence.Table;
-
-@Entity
-@Table(name="cat_information")
-public class Information extends Catalog {
-}
--- src/main/java/kg/gov/mf/loan/doc/model/Organization.java
+++
@@ -1,8 +0,0 @@
-package kg.gov.mf.loan.doc.model;
-import javax.persistence.Entity;
-import javax.persistence.Table;
-
-@Entity
-@Table(name="cat_organization")
-public class Organization extends Catalog {
-}
