fcf144842f900b84d8a8cc4cf9baf8fe5fe8dc23
NURLAN <regalix@gmail.com>
2018-05-28 19:08:44 +0600

    28.05.2018

--- src/main/java/kg/gov/mf/loan/doc/dao/DocumentDaoImpl.java
+++ src/main/java/kg/gov/mf/loan/doc/dao/DocumentDaoImpl.java
@@ -1,23 +1,19 @@
 package kg.gov.mf.loan.doc.dao;
 
-import kg.gov.mf.loan.admin.sys.dao.UserDao;
 import kg.gov.mf.loan.doc.model.Document;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
-
 import java.util.List;
 
 @Repository
 public class DocumentDaoImpl extends GenericDaoImpl<Document> implements DocumentDao
 {
     private DocumentTypeDao documentTypeDao;
-    private UserDao userDao;
 
     @Autowired
-    public DocumentDaoImpl(DocumentTypeDao documentTypeDao, UserDao userDao) {
+    public DocumentDaoImpl(DocumentTypeDao documentTypeDao) {
         this.documentTypeDao = documentTypeDao;
-        this.userDao = userDao;
     }
 
     @Override
@@ -26,7 +22,7 @@ public class DocumentDaoImpl extends GenericDaoImpl<Document> implements Documen
         String query = "from Document d where owner = :owner and d.documentType = :documentType and documentState < 8";
         return getCurrentSession().createQuery(query)
                 .setParameter("documentType", documentTypeDao.getByInternalName(documentType))
-                .setParameter("owner", userDao.findById(userId).getId())
+                .setParameter("owner", userId)
                 .list();
     }
 
@@ -35,7 +31,7 @@ public class DocumentDaoImpl extends GenericDaoImpl<Document> implements Documen
     public List getArchivedDocuments(Long userId) {
         String query = "from Document where owner = :owner and documentState = 8";
         return getCurrentSession().createQuery(query)
-                .setParameter("owner", userDao.findById(userId).getId())
+                .setParameter("owner", userId)
                 .list();
     }
 }
